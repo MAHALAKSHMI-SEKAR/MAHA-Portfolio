31ca60c1777db023382b438c4fcf4c21670b9c5e
Mahalakshmi <mahanushya3001@gmail.com>
2026-09-24 16:12:03 +0530

    updated portfolio

--- portfolio/portfolio/src/main/java/com/backend/portfolio/ContactController.java
+++ portfolio/portfolio/src/main/java/com/backend/portfolio/ContactController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController 
 @RequestMapping("/api/contact")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173,https://maha-portfolio-qrcov7c0u-mahalakshmi-sekar.vercel.app")
 public class ContactController {
 
     @Autowired
