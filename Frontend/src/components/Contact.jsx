936916adf6161b8df8068d5f65b007bd4023fe68
Mahalakshmi <mahanushya3001@gmail.com>
2026-09-24 18:28:25 +0530

    changed

--- Frontend/src/components/Contact.jsx
+++ Frontend/src/components/Contact.jsx
@@ -37,7 +37,7 @@ const handleSubmit = async (e) => {
 
   try {
     const response = await fetch(
-      'https://maha-portfolio-a7x7.onrender.com/api/contact',
+      `${import.meta.env.VITE_API_URL || '/api/contact'}` ,
       {
         method: 'POST',
         headers: {
